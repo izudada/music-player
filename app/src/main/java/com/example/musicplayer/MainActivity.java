4d053461ec5f3c622fd179d5e7825e346dc9decc
Anthony <tonyudeagbala@gmail.com>
2022-07-05 20:45:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //  Get all text views
+        TextView reggeaTextView = (TextView) findViewById(R.id.reggea);
+        TextView bluesTextView = (TextView) findViewById(R.id.blues);
+        TextView hiphopTextView = (TextView) findViewById(R.id.hiphop);
+        TextView classicalTextView = (TextView) findViewById(R.id.classical);
+
+        // Onclick listener on reggeaTextView
+        reggeaTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reggeaActivity = new Intent(MainActivity.this, ReggeaActivity.class);
+                startActivity(reggeaActivity);
+            }
+        });
+    }
+}
