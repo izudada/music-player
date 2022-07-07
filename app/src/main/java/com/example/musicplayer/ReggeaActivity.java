fd5cdb9e675f1f52c357d0bf4228937cec76d898
Anthony <tonyudeagbala@gmail.com>
2022-07-07 23:57:47 +0100

    Change variable name

--- app/src/main/java/com/example/musicplayer/ReggeaActivity.java
+++ app/src/main/java/com/example/musicplayer/ReggeaActivity.java
@@ -20,25 +20,25 @@ public class ReggeaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.music_list);
 
-        ArrayList<Music> songs = new ArrayList<Music>();
+        ArrayList<Music> reggeaSongs = new ArrayList<Music>();
 
-        songs.add(new Music("Available", "Patoranking"));
-        songs.add(new Music("Worth His Weight in Gold (Rally Round)", "Steel Pulse"));
-        songs.add(new Music("I'm Still In Love With You", "Sean Paul"));
-        songs.add(new Music("No Weapon Formed Against Me Shall Prosper", "Africason"));
-        songs.add(new Music("Footprints", "T.O.K."));
-        songs.add(new Music("I've Got You Babe", "Lucky Dube"));
-        songs.add(new Music("Love and Affection", "Pressure"));
-        songs.add(new Music("I No Fit Shout", "Busy Signal"));
-        songs.add(new Music("Iron Lion Zion", "Bob Marley & The Wailers"));
-        songs.add(new Music("Sweet Sensation", "Orezi"));
+        reggeaSongs.add(new Music("Available", "Patoranking"));
+        reggeaSongs.add(new Music("Worth His Weight in Gold (Rally Round)", "Steel Pulse"));
+        reggeaSongs.add(new Music("I'm Still In Love With You", "Sean Paul"));
+        reggeaSongs.add(new Music("No Weapon Formed Against Me Shall Prosper", "Africason"));
+        reggeaSongs.add(new Music("Footprints", "T.O.K."));
+        reggeaSongs.add(new Music("I've Got You Babe", "Lucky Dube"));
+        reggeaSongs.add(new Music("Love and Affection", "Pressure"));
+        reggeaSongs.add(new Music("I No Fit Shout", "Busy Signal"));
+        reggeaSongs.add(new Music("Iron Lion Zion", "Bob Marley & The Wailers"));
+        reggeaSongs.add(new Music("Sweet Sensation", "Orezi"));
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
         // adapter knows how to create layouts for each item in the list, using the
         // simple_list_item_1.xml layout resource defined in the Android framework.
         // This list item layout contains a single {@link TextView}, which the adapter will set to
         // display a single word.
-        MusicAdapter adapter = new MusicAdapter(this, songs);
+        MusicAdapter adapter = new MusicAdapter(this, reggeaSongs);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
